ef70b031fcba03474621703d281642458317f5ba
oskar-duda <o.duda@poczta.onet.pl>
2019-05-05 19:40:23 +0200

    Update readme. Refactored code. Introduced xgboost. Fixed category encoding.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# natural-language-processing
-First steps in the field of natural language processing ...
+# Natural language processing
+## Installation
+```bash
+git clone https://github.com/OskarDuda/natural-language-processing.git
+cd natural-language-processing
+```
+
+creating a Python virtual environment:
+```bash
+virtualenv venv -p python3
+source venv/bin/activate
+```
+
+intalling dependencies:
+```bash
+pip3 install -r requirements.txt
+```
+
+## How to run it
+If virtual environment has been created:
+```bash
+source venv/bin/activate
+```
+
+running the script:
+```bash
+python3 gender_prediction_by_tweets.py 
+```
+
+the resulting score of the prediction will be printed in the console. 
+Current version doesn't support model selection.
--- gender_prediction_by_tweets.py
+++ gender_prediction_by_tweets.py
@@ -1,20 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 24 19:45:02 2017
-
-@author: oskar
-"""
-
+import category_encoders as ce
 import string
 import numpy as np
 import pandas as pd
 from collections import Counter
-import seaborn as sns
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import cross_val_score as cvs
-
+from sklearn.model_selection import cross_val_score as cvs, train_test_split
+import xgboost
 
 TMP = 0
 
@@ -22,9 +14,9 @@ TMP = 0
 #         'me','your','im','on','we','that','was','they','by','so','he',
 #         'as','she','this'}
 
-punctuation = set(string.punctuation)
-letters = set(string.ascii_letters)
-numbers = set(string.digits)
+PUNCTUATION = set(string.punctuation)
+LETTERS = set(string.ascii_letters)
+NUMBERS = set(string.digits)
 
 def remove_hashtag(input_text):
     words = input_text.split()
@@ -33,14 +25,14 @@ def remove_hashtag(input_text):
             w = w[1:]
     return ' '.join(words)
 
-def remove_interpunction(input_text):
+def remove_punctuation(input_text):
     global TMP
     
     if input_text:
         words = input_text.split()
         new_words = []
         for w in words:
-            new_words.append(''.join(ch for ch in w if ch not in punctuation))
+            new_words.append(''.join(ch for ch in w if ch not in PUNCTUATION))
         return ' '.join(new_words)
     else:
         return input_text
@@ -62,80 +54,93 @@ def find_most_common(input_text,n=3):
             return word_counter[0][0]
     else:
         return 0
-    
-
-path = 'test.csv'
-df = pd.read_csv(path, encoding = "ISO-8859-1")
-df = df[['gender','text']]
-df = df[(df['gender'] == 'male') | (df['gender'] == 'female') ]
-
-df['raw_text'] = [(remove_hashtag(x.lower())) for x in df['text']]   
-print('Hashtags removed: ') 
-
-df['raw_text'] = [remove_ats(x) for x in df['raw_text']]   
-print('@ removed: ')
-
-df['raw_text'] = [remove_interpunction(x) for x in df['raw_text']]   
-print('Interpunction removed: ')
-
-df['most_common'] = [find_most_common(x,1) for x in df['raw_text']] 
-print('Most common used word in each text found ')
-
-df['text_length'] = [len(x) for x in df['text']]
-
-#average word length
-df['avg_word_length'] = [np.average([len(x) for x in a.split()]) for a in df['raw_text']]
-
-#removing tweets with average word length higher or equal to 18
-df = df[df['avg_word_length']<18]
-
-#dictionary with most common words occurences
-d = dict(zip(list(df['most_common'].value_counts().index),
-             list(df['most_common'].value_counts().data)))
-    
-#changing occurences into frequencies in d
-tmp = sum(d.values())
-for key in d:
-    d[key] = d[key]/tmp
-    
-df['frequency'] = [d[x] for x in df['most_common']]
-print("Most common words found and binarized")  
-
-#checking if interpunction is being used
-df['interpunction'] = [bool(set(df.loc[i]['text']) & punctuation) for i in df.index]
-print("Interpunction checked")  
-
-#counting words
-df['words_number'] = [len(x.split()) for x in df['text']]
-print('Words counted\n')  
 
 
-##Gender prediction
+def load_data(path):
+    df = pd.read_csv(path, encoding="ISO-8859-1")
+    df = df[['gender','text']]
+    df = df[(df['gender'] == 'male') | (df['gender'] == 'female') ]
+    return df
 
-clf_attributes = ['text_length','interpunction','frequency','words_number','avg_word_length']
-clf_to_predict = 'gender'
 
-#Label encoding
-print("Labels are being encoded ...")
-le = LabelEncoder()
-data = []
-for a in clf_attributes:
-    le.fit(df[a])
-    data.append(le.transform(df[a]))
-    print(a + ' label encoded')
+def extract_features(data):
+    data = data.copy()
+    data['raw_text'] = [(remove_hashtag(x.lower())) for x in data['text']]
+    print('Hashtags removed: ')
 
-#transpose data matrix
-data = list(map(list, zip(*data)))
-target = df[clf_to_predict]
+    data['raw_text'] = [remove_ats(x) for x in data['raw_text']]
+    print('@ removed: ')
 
-print("All labels are encoded\n")  
-print("Classification:")
-clf =  RandomForestClassifier()
-clf.fit(data,target)
-feat_imp = clf.feature_importances_
-indices = np.argsort(feat_imp)[::-1]
-for i in indices:
-    print("Feature importance of {a}: {b:4.2f}%".format(a = clf_attributes[i],b = 100*feat_imp[i]))
-print()
-score = 100*np.average(cvs(clf, data, target, cv=10))
-print("Cross validation score is: % 4.2f" % score + "%")
+    data['raw_text'] = [remove_punctuation(x) for x in data['raw_text']]
+    print('Punctuation removed: ')
+
+    data['most_common'] = [find_most_common(x, 1) for x in data['raw_text']]
+    print('Most common used word in each text found ')
+
+    data['text_length'] = [len(x) for x in data['text']]
+
+    data['avg_word_length'] = [np.average([len(x) for x in a.split()]) for a in data['raw_text']]
+
+    data = data[data['avg_word_length'] < 18]  # more than 99% of english words shorter than 18 letters
+
+    #dictionary with most common words occurences
+    d = dict(zip(list(data['most_common'].value_counts().index),
+                 list(data['most_common'].value_counts().data)))
+
+    #changing occurences into frequencies in d
+    tmp = sum(d.values())
+    for key in d:
+        d[key] = d[key]/tmp
+
+    data['frequency'] = [d[x] for x in data['most_common']]
+    print("Most common words found and binarized")
+
+    #checking if punctuation is being used
+    data['punctuation'] = [bool(set(data.loc[i]['text']) & PUNCTUATION) for i in data.index]
+    print("Punctuation checked")
+
+    #counting words
+    data['words_number'] = [len(x.split()) for x in data['text']]
+    print('Words counted\n')
+
+    return data
+
+
+def encode_labels(data, X_columns):
+    data = data[X_columns].copy()
+    print("Labels are being encoded ...")
+    le = ce.OrdinalEncoder()
+    data = le.fit_transform(data)
+    return data
+
+
+def build_model(X, y):
+    clf = xgboost.XGBClassifier(n_estimators=200)
+    clf.fit(X, y)
+    return clf
+
+
+def evaluate_model(clf, X, y):
+    feat_imp = dict(zip(X.columns, clf.feature_importances_))
+    for feature in feat_imp.keys():
+        print("Feature importance of {}: {:4.2f}%".format(feature, 100*feat_imp[feature]))
+    score = roc_auc_score(y, clf.predict(X))
+    print("\nAUC score is: % 4.2f" % score + "%")
+    return score, feat_imp
+
+
+def main():
+    path = 'Data/test.csv'
+    df = load_data(path)
+    df = extract_features(df)
+    df['is_male'] = (df['gender'] == 'male').astype(int)
+    X_features = ['most_common', 'text_length', 'punctuation', 'frequency', 'words_number', 'avg_word_length']
+    y_features = 'is_male'
+    df[X_features] = encode_labels(df, X_features)
+    X_train, X_test, y_train, y_test = train_test_split(df[X_features], df[y_features])
+    clf = build_model(X_train, y_train)
+    score, feature_importance = evaluate_model(clf, X_test, y_test)
+
+
+if __name__ == '__main__':
+    main()
